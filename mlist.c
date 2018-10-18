b9425b56a42000217ff305f7f69093139c0a6642
unknown <gzhou2@stuy.edu>
2018-10-17 22:01:09 -0400

    edited delete and insert_order

--- mlist.c
+++ mlist.c
@@ -32,32 +32,33 @@ struct song_node * free_list(struct song_node * x){
 }
 
 struct song_node * insert_order(struct song_node * x,char* s1,char* s2){
-  if(x){
-   return insert_front(x,s1,s2);
+  if(x == NULL){
+    return insert_front(x,s1,s2);
   }
-  struct song_node * new_node = malloc(sizeof(struct song_node));
-  strcpy(new_node->name, s1);
-  strcpy(new_node->artist, s2);
+  struct song_node * head = x;
   struct song_node * before = NULL;
-  while(x){
-    if(strcmp(x->artist,new_node->artist) == 0){
-      if(strcmp(x->name,new_node->name) <= 0){
-	before->next = new_node;
-	new_node->next = x;
+  struct song_node * new_node;
+  
+  while( head != NULL && strcmp(head->artist,s2) > 0){
+    before = head;
+    head = head->next;
+  }
+  
+  while(head != NULL && strcmp(head->artist,s2) == 0){
+    if(strcmp(head->name,s1) <= 0){
+      new_node = insert_front(head.s1,s2);
+      if(before == NULL){
 	return new_node;
       }
-    }
-    if(strcmp(new_node->artist,x->artist) < 0){
       before->next = new_node;
-      new_node->next = x;
-      return new_node;
     }
-    before = x;
-    x= x->next;
+    before = head;
+    head->next = head;
   }
-  return new_node;
+  new_node = insert_front(head,s1,s2);
+  before->next = new_node;
+  return x;
 }
-
 struct song_node * search_both(struct song_node * x, char * s1, char * s2){
   struct song_node * head = x;
   while(head){
@@ -98,16 +99,23 @@ struct song_node * random_node(struct song_node * x){
 
 struct song_node * delete_node(struct song_node * x, char * s1,char * s2){
   struct song_node * head = x;
-  struct song_node * before;
-  struct song_node * after;
-  while(head){
-    if(!(strcmp(head->name,s1)) && !(strcmp(head->artist,s2))){
-      
-      return head;
-    }
-    head = head -> next;
+  struct song_node * next_node;
+  if(strcmp(head->name,s1)== 0 && strcmp(head->artist,s2) == 0){
+    struct song_node *new_start = head->next;
+    free(head);
+    return new_start;
   }
-  return 0;
+  while(head->next != NULL){
+    next_node = head->next;
+    if(strcmp(next_node->name,s1)==0 && strcmp(next_node->artist,s2) == 0){
+      struct song_node * temp = next_node;
+      free(next_node);
+      head->next = temp -> next;
+    }
+    head = head->next;
+  }
+  printf("Song not found\n");
+  return head;
 }
 
 
